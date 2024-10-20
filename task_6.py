d7a91d911ec8c085bea90eaad97d3fe94cfc5aa0
Kubanychbek Tursunbekov <kubanychbek.tursunbekov.alt@gmail.com>
2024-10-20 22:08:47 +0300

    add task_6

---
+++ task_6.py
@@ -0,0 +1,16 @@
+# Напишите функцию, которая считает количество цифр в числе.
+# Число передаётся в качестве параметра. Из функции нужно
+# вернуть полученное количество цифр. Например, если
+# передали 3456, количество цифр будет 4.
+
+
+def number_of_digits_in_num(num):
+    number_of_digits = 0
+    for i in range(len(num)):
+        if num[i].isdigit():
+            number_of_digits += 1
+    print(number_of_digits)
+
+
+ask_for_digit = input("Введите число: ")
+number_of_digits_in_num(ask_for_digit)
