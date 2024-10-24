9def1ce4587c888d7bce504aed0543717c32a136
Kubanychbek Tursunbekov <kubanychbek.tursunbekov.alt@gmail.com>
2024-10-24 21:25:49 +0300

    update the task_5

--- task_5.py
+++ task_5.py
@@ -5,25 +5,34 @@
 # поменять местами.
 
 
+class RangeError(Exception):
+    pass
+
+
 def product_of_nums(a, b):
-    start = min(a, b)
-    end = max(a, b)
-    product = 1
-    if a == b:
-        print("Границы диапазона одинаковые")
-        product -= 1
-    elif a > b:
-        print("Границы диапазона перепутаны, поэтому мы их поменяли местами.")
-        print()
-        for i in range(start, end + 1):
-            product *= i
-    else:
-        for i in range(a, b + 1):
-            product *= i
-    print(f"Произведение чисел от {start} до {end} равно:\n{product}")
+    try:
+        start = min(a, b)
+        end = max(a, b)
+        product = 1
+        if a == b:
+            raise RangeError("Границы диапазона одинаковые")
+        elif a > b:
+            print("Границы диапазона перепутаны, поэтому мы их поменяли местами.")
+            print()
+            for i in range(start, end + 1):
+                product *= i
+        else:
+            for i in range(a, b + 1):
+                product *= i
+        print(f"Произведение чисел от {start} до {end} равно:\n{product}")
+    except RangeError as err:
+        print(err)
 
 
-start_of_range = int(input("Введите начало диапазона: "))
-end_of_range = int(input("Введите конец диапазона: "))
-
-product_of_nums(start_of_range, end_of_range)
+try:
+    start_of_range = int(input("Введите начало диапазона: "))
+    end_of_range = int(input("Введите конец диапазона: "))
+except ValueError:
+    print("Введите только целое число")
+else:
+    product_of_nums(start_of_range, end_of_range)
