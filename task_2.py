be7c3a0d3cb63ea37fd716cd9c4c07af46f57ba8
Kubanychbek Tursunbekov <kubanychbek.tursunbekov.alt@gmail.com>
2024-10-24 19:38:34 +0300

    update the task_2

--- task_2.py
+++ task_2.py
@@ -6,7 +6,7 @@ def even_nums():
     x = int(input("Enter start of range: "))
     y = int(input("Enter end of range: "))
 
-    for i in range(x, y):
+    for i in range(x, y + 1):
         if i % 2 == 0:
             print(i)
 
