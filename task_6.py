86b80320efa85da5430d81d61b933ea74b63cfce
Kubanychbek Tursunbekov <kubanychbek.tursunbekov.alt@gmail.com>
2024-10-25 22:21:04 +0300

    update task_6

--- task_6.py
+++ task_6.py
@@ -5,12 +5,17 @@
 
 
 def number_of_digits_in_num(num):
+    into_str = str(num)
     number_of_digits = 0
-    for i in range(len(num)):
-        if num[i].isdigit():
+    for i in range(len(into_str)):
+        if into_str[i].isdigit():
             number_of_digits += 1
     print(f"Количество цифр в числе {number_of_digits}")
 
 
-ask_for_digit = input("Введите число: ")
-number_of_digits_in_num(ask_for_digit)
+try:
+    ask_for_digit = int(input("Введите число: "))
+except ValueError:
+    print("Введите только целое число")
+else:
+    number_of_digits_in_num(ask_for_digit)
