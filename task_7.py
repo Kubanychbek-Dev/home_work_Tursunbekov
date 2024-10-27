1751aec0698673fe855cbcfea9cfec3ee29b6089
Kubanychbek Tursunbekov <kubanychbek.tursunbekov.alt@gmail.com>
2024-10-27 22:26:54 +0300

    update task_7

--- task_7.py
+++ task_7.py
@@ -3,12 +3,21 @@
 # число палиндром нужно вернуть из функции true, иначе false.
 
 
-def checking_number_for_palindrome(num):
+def checking_number_for_palindrome(st):
+    num = str(st)
     if num == num[::-1]:
         print("True")
     else:
         print("False")
 
 
-ask_for_num = input("Введите число для проверки на палиндромом: ")
-checking_number_for_palindrome(ask_for_num)
+def ask_num_for_check():
+    try:
+        ask_for_num = int(input("Введите число для проверки на палиндромом: "))
+    except ValueError:
+        print("Введите только целое число")
+    else:
+        checking_number_for_palindrome(ask_for_num)
+
+
+ask_num_for_check()
